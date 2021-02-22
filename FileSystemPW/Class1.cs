cd26abb3574fd71ea462ee37d342516e3a1812ff
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-22 10:42:14 +0600

    Добавьте файлы проекта.

---
+++ FileSystemPW/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileSystemPW
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets\pwtask2.txt";
+            using (StreamWriter stream = new StreamWriter(path))
+            {
+                stream.WriteLine("Farhat");
+                stream.WriteLine("Bolatuly");
+                stream.WriteLine("35");
+            }
+        }
+    }
+}
---
+++ FileSystemPW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystemPW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> stat = new Dictionary<char, int>();
+            string path = @"C:\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets\2.txt";
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                string data = System.Text.Encoding.Unicode.GetString(bytes);
+                foreach (char symbol in data)
+                {
+                    if (stat.ContainsKey(symbol))
+                    {
+                        stat[symbol]++;
+                    }
+                    else
+                    {
+                        stat[symbol] = 0;
+                    }
+                }
+            }
+            foreach (KeyValuePair<char, int> item in stat)
+            {
+                Console.WriteLine(item.Key + "\t\t" + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
